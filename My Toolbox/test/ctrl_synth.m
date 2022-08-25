b7bdc6ef44da023639cf62d2184e8d70c96d156a
Charles <charles.sirois14@gmail.com>
2022-08-25 13:11:51 +0200

    Tuning from Simulink

--- My Toolbox/test/ctrl_synth.m
+++ My Toolbox/test/ctrl_synth.m
@@ -21,10 +21,21 @@ err_m = err_m(1:end-1);
 err_m_dot = split(sprintf('err_m_dot(%i);', 1:n), ';');
 err_m_dot = err_m_dot(1:end-1);
 
-q = split(sprintf('q(%i);', 1:N), ';');
-q = q(1:end-1);
-q_dot = split(sprintf('q_dot(%i);', 1:N), ';');
-q_dot = q_dot(1:end-1);
+qb = split(sprintf('qb(%i);', 1:6), ';');
+qb = qb(1:end-1);
+qb_dot = split(sprintf('qb_dot(%i);', 1:6), ';');
+qb_dot = qb_dot(1:end-1);
+
+qm = split(sprintf('qm(%i);', 1:n), ';');
+qm = qm(1:end-1);
+qm_dot = split(sprintf('qm_dot(%i);', 1:n), ';');
+qm_dot = qm_dot(1:end-1);
+
+tau_b = split(sprintf('tau_b(%i);', 1:6), ';');
+tau_b = tau_b(1:end-1);
+tau_m = split(sprintf('tau_m(%i);', 1:n), ';');
+tau_m = tau_m(1:end-1);
+
 
 % Coordinated Base controller Synthesis
 A = [zeros(N, N), eye(N);
@@ -36,9 +47,10 @@ C = eye(2*N);
 D = zeros(2*N, N);
 
 sys = ss(A, B, C, D);
-sys.InputName = 'tau';
+sys.InputName = [tau_b; tau_m];
 sys.StateName = x;
-sys.OutputName = [q; q_dot];
+y = [qb; qm; qb_dot; qm_dot];
+sys.OutputName = y;
 
 %% Base Controller
 B_err = AnalysisPoint('B_err');
@@ -77,16 +89,71 @@ Kd_m.Gain.Free = eye(n);
 Kd_m.InputName = 'err_m_dot';
 Kd_m.OutputName = 'ud_m';
 
-Dm = tunableGain('M_Decoupler',eye(2*n));
-Dm.Gain.Free = zeros(2*n);
-Dm.InputName = 'M_err';
-Dm.OutputName = [err_m; err_m_dot];
-
 sum_mC = sumblk('tau_m = up_m + ud_m',n);
 
-Cm = connect(Dm ,Kp_m ,Kd_m, sum_mC, 'M_err', 'tau_m');
+Cm = connect(Kp_m ,Kd_m, sum_mC, [err_m; err_m_dot], 'tau_m');
 
 %% Full Syst
 
-sum_mE = sumblk('tau_m = up_m + ud_m',n);
+sum_b_err = sumblk('err_b = qb_ref - qb', 6);
+sum_b_err.OutputName = err_b;
+sum_m_err = sumblk('err_m = qm_ref - qm', n);
+
+sum_b_err_dot = sumblk('err_b_dot = qb_dot_ref - qb_dot', 6);
+sum_m_err_dot = sumblk('err_m_dot = qm_dot_ref - qm_dot', n);
+
+CL = connect(sum_b_err, sum_m_err, sum_b_err_dot, sum_m_err_dot, sys, ...
+            {'qb_ref', 'qb_dot_ref', 'qm_ref', 'qm_dot_ref'}, ...
+            y);
+
+
+
+
+
+
+%% Init
+clc
+load 'SC_2DoF.mat'
+sc.homeConfig;
+n = sc.NumActiveJoints;
+N = n + 6;
+
+x0 = [sc.q; sc.q_dot];
+
+f0 = [0; 0.1; 0];     % Forces on base [fx, fy, fz], in base frame
+n0 = [0; 0; 0];     % Torques on base [nx, ny, nz], in base frame
+tau_qm = [0; 0];      % Joint torques
+
+
+% Coordinated Base controller Synthesis
+A = [zeros(N, N), eye(N);
+     zeros(N, N), zeros(N, N)];
+
+B = [zeros(N, N); sc.H^-1];
+
+C = eye(2*N);
+D = zeros(2*N, N);
+
+%% Tuning
+mdl = 'Coordinated_ctrl_tuning';
+st0 = slTuner(mdl,{'Cp_b','Cd_b', 'Cp_m', 'Cd_m'});
+addPoint(st0,{'qb_ref','qb'});
+
+% T = getIOTransfer(st,'qb_ref','qb');
+% stepplot(T)
+
+
+req1 = TuningGoal.Tracking('qb_ref','qb',1);
+
+[st,fSoft,~,info] = systune(st0, req1);
+
+T = getIOTransfer(st,'qb_ref','qb');
+stepplot(T)
+
+% req2 = TuningGoal.Gain('delta fin','delta fin',tf(25,[1 0]));
+% req3 = TuningGoal.Margins('delta fin',7,45);
+% max_gain = frd([2 200 200],[0.02 2 200]);
+% req4 = TuningGoal.Gain('delta fin','az',max_gain);
+
+
 
