b6cf8ca8a12cefd968b82a774d5158d05924f4f1
Charles <charles.sirois14@gmail.com>
2022-08-25 15:39:48 +0200

    Inner loop controller

--- My Toolbox/test/ctrl_synth.m
+++ My Toolbox/test/ctrl_synth.m
@@ -1,129 +1,12 @@
 %% Init
 clc
-load 'SC_2DoF.mat'
-sc.homeConfig;
-n = sc.NumActiveJoints;
-N = n + 6;
-
-q_name = cellstr(arrayfun(@string, sc.q_symb));
-q_dot_name = cellstr(arrayfun(@string, sc.q_dot_symb));
-x = [q_name; q_dot_name];
-
-err_b = split(sprintf('err_b(%i);', 1:6), ';');
-err_b = err_b(1:6);
-
-err_b_dot = split(sprintf('err_b_dot(%i);', 1:6), ';');
-err_b_dot = err_b_dot(1:6);
-
-err_m = split(sprintf('err_m(%i);', 1:n), ';');
-err_m = err_m(1:end-1);
-
-err_m_dot = split(sprintf('err_m_dot(%i);', 1:n), ';');
-err_m_dot = err_m_dot(1:end-1);
-
-qb = split(sprintf('qb(%i);', 1:6), ';');
-qb = qb(1:end-1);
-qb_dot = split(sprintf('qb_dot(%i);', 1:6), ';');
-qb_dot = qb_dot(1:end-1);
-
-qm = split(sprintf('qm(%i);', 1:n), ';');
-qm = qm(1:end-1);
-qm_dot = split(sprintf('qm_dot(%i);', 1:n), ';');
-qm_dot = qm_dot(1:end-1);
-
-tau_b = split(sprintf('tau_b(%i);', 1:6), ';');
-tau_b = tau_b(1:end-1);
-tau_m = split(sprintf('tau_m(%i);', 1:n), ';');
-tau_m = tau_m(1:end-1);
-
-
-% Coordinated Base controller Synthesis
-A = [zeros(N, N), eye(N);
-     -sc.H^-1, zeros(N, N)];
-
-B = [zeros(N, N); sc.H^-1];
-
-C = eye(2*N);
-D = zeros(2*N, N);
-
-sys = ss(A, B, C, D);
-sys.InputName = [tau_b; tau_m];
-sys.StateName = x;
-y = [qb; qm; qb_dot; qm_dot];
-sys.OutputName = y;
-
-%% Base Controller
-B_err = AnalysisPoint('B_err');
-Tau_b = AnalysisPoint('Tau_b');
-
-Kp_b = tunableGain('gainblock',6,6);
-Kp_b.Gain.Free = eye(6);
-Kp_b.InputName = 'err_b';
-Kp_b.OutputName = 'up_b';
-
-Kd_b = tunableGain('gainblock',6,6);
-Kd_b.Gain.Free = eye(6);
-Kd_b.InputName = 'err_b_dot';
-Kd_b.OutputName = 'ud_b';
-
-Db = tunableGain('Base_Decoupler',eye(12));
-Db.Gain.Free = zeros(12);
-Db.InputName = 'B_err';
-Db.OutputName = [err_b; err_b_dot];
-
-sum_bC = sumblk('tau_b = up_b + ud_b',6);
-
-Cb = connect(Kp_b ,Kd_b, sum_bC, [err_b; err_b_dot], 'tau_b');
-
-%% Manip Controller
-M_err = AnalysisPoint('M_err');
-Tau_M = AnalysisPoint('Tau_M');
-
-Kp_m = tunableGain('gainblock',n,n);
-Kp_m.Gain.Free = eye(n);
-Kp_m.InputName = 'err_m';
-Kp_m.OutputName = 'up_m';
-
-Kd_m = tunableGain('gainblock',n,n);
-Kd_m.Gain.Free = eye(n);
-Kd_m.InputName = 'err_m_dot';
-Kd_m.OutputName = 'ud_m';
-
-sum_mC = sumblk('tau_m = up_m + ud_m',n);
-
-Cm = connect(Kp_m ,Kd_m, sum_mC, [err_m; err_m_dot], 'tau_m');
-
-%% Full Syst
-
-sum_b_err = sumblk('err_b = qb_ref - qb', 6);
-sum_b_err.OutputName = err_b;
-sum_m_err = sumblk('err_m = qm_ref - qm', n);
-
-sum_b_err_dot = sumblk('err_b_dot = qb_dot_ref - qb_dot', 6);
-sum_m_err_dot = sumblk('err_m_dot = qm_dot_ref - qm_dot', n);
-
-CL = connect(sum_b_err, sum_m_err, sum_b_err_dot, sum_m_err_dot, sys, ...
-            {'qb_ref', 'qb_dot_ref', 'qm_ref', 'qm_dot_ref'}, ...
-            y);
-
-
-
-
-
-
-%% Init
-clc
+warning off
 load 'SC_2DoF.mat'
 sc.homeConfig;
 n = sc.NumActiveJoints;
 N = n + 6;
 
 x0 = [sc.q; sc.q_dot];
-
-f0 = [0; 0.1; 0];     % Forces on base [fx, fy, fz], in base frame
-n0 = [0; 0; 0];     % Torques on base [nx, ny, nz], in base frame
-tau_qm = [0; 0];      % Joint torques
-
 
 % Coordinated Base controller Synthesis
 A = [zeros(N, N), eye(N);
@@ -136,19 +19,60 @@ D = zeros(2*N, N);
 
 %% Tuning
 mdl = 'Coordinated_ctrl_tuning';
-st0 = slTuner(mdl,{'Cp_b','Cd_b', 'Cp_m', 'Cd_m'});
+st0 = slTuner(mdl,{'Kp_b','Kd_b', 'Kp_m', 'Kd_m'});
+maxKpb = 1000;
+maxKdb = 1000;
+maxKpm = 1000;
+maxKdm = 1000;
+
+G1 = tunableGain('Kp_b', eye(6));
+G1.Gain.Free = eye(6);
+G1.Gain.Maximum = diag(maxKpb);
+
+G2 = tunableGain('Kd_b', eye(6));
+G2.Gain.Free = eye(6);
+G2.Gain.Maximum = diag(maxKdb);
+
+G3 = tunableGain('Kp_m', eye(n));
+G3.Gain.Free = eye(n);
+G3.Gain.Maximum = diag(maxKpm);
+
+G4 = tunableGain('Kd_m', eye(n));
+G4.Gain.Free = eye(n);
+G4.Gain.Maximum = diag(maxKdm);
+
+setBlockParam(st0,'Kp_b',G1,'Kd_b',G2,'Kp_m',G3, 'Kd_m', G4);
+% getBlockParam(st0, 'Kp_b').Gain.Value
+
 addPoint(st0,{'qb_ref','qb'});
+addPoint(st0,{'qb_dot_ref','qb_dot'});
+addPoint(st0,{'qm_ref','qm'});
+addPoint(st0,{'qm_dot_ref','qm_dot'});
 
 % T = getIOTransfer(st,'qb_ref','qb');
 % stepplot(T)
 
 
-req1 = TuningGoal.Tracking('qb_ref','qb',1);
+req1 = TuningGoal.Tracking('qb_ref','qb',10);
+req2 = TuningGoal.Tracking('qb_dot_ref','qb_dot',1);
+req3 = TuningGoal.Tracking('qm_ref','qm',1);
+req4 = TuningGoal.Tracking('qm_dot_ref','qm_dot',1);
 
-[st,fSoft,~,info] = systune(st0, req1);
+% [st,fSoft,~,info] = systune(st0, [req1, req2, req3, req4]);
+[st,fSoft,~,info] = systune(st0, [req1, req3]);
 
-T = getIOTransfer(st,'qb_ref','qb');
-stepplot(T)
+% T1= getIOTransfer(st,'qb_ref','qb');
+% stepplot(T1)
+% 
+% T2 = getIOTransfer(st,'qm_ref','qm');
+% stepplot(T2)
+
+showTunable(st)
+
+Kp_b = diag(getBlockParam(st, 'Kp_b').Gain.Value);
+Kd_b = diag(getBlockParam(st, 'Kd_b').Gain.Value);
+Kp_m = diag(getBlockParam(st, 'Kp_m').Gain.Value);
+Kd_m = diag(getBlockParam(st, 'Kd_m').Gain.Value);
 
 % req2 = TuningGoal.Gain('delta fin','delta fin',tf(25,[1 0]));
 % req3 = TuningGoal.Margins('delta fin',7,45);
@@ -156,4 +80,7 @@ stepplot(T)
 % req4 = TuningGoal.Gain('delta fin','az',max_gain);
 
 
+% writeBlockValue(st)
+% showTunable(st)
 
+warning on
--- My Toolbox/test/traj_tracking_test.m
+++ My Toolbox/test/traj_tracking_test.m
@@ -1,14 +1,51 @@
 %% Initialization
 clc
 load 'SC_2DoF.mat'
-
-f0 = [0; 0; 0];     % Forces on base [fx, fy, fz], in base frame
-n0 = [0; 0; 0];     % Torques on base [nx, ny, nz], in base frame
-tau_qm = [0; 0.1];      % Joint torques
-
 sc.homeConfig();
 
 tTree = sc.forwardKinematics();
 [~, p0] = tr2rt(tTree.endeffector);
 
 traj = squareTraj(p0, 0.5, 10, 101, 'plane', 'xy');
+
+%% Animation
+close all
+filename = 'gifTest2.gif';
+saveAnim = false;
+
+% Parameters
+dim = [.2 .5 .3 .3];
+simRes = out;
+framesPerSecond = length(simRes.tout)/simRes.tout(end);
+str = sprintf('t = %.2fs', 0);
+sc.q = simRes.q.Data(:, :, 1);
+
+% First frame
+h_annot = annotation('textbox', dim,'String', str,'FitBoxToText','on');
+sc.show('preserve', false, 'fast', true, 'visuals', 'on');
+drawnow
+
+
+f = getframe;
+[im,map] = rgb2ind(f.cdata,256,'nodither');
+im(1,1,1,20) = 0;
+
+for i = 1:length(simRes.q.Time)
+    sc.q = simRes.q.Data(:, :, i);
+    sc.show('preserve', false, 'fast', true, 'visuals', 'on');
+    curT = simRes.q.Time(i);
+    str = sprintf('t = %.2fs', curT);    
+    h_annot.set('String',str);
+    drawnow
+    
+    if saveAnim
+        f = getframe;
+        im(:,:,1,i) = rgb2ind(f.cdata,map,'nodither');
+    end
+end
+
+if saveAnim
+    imwrite(im,map,filename,'DelayTime',1/framesPerSecond,'LoopCount',inf)
+end
+
+sc.homeConfig;
